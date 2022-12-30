82b7937fb2143f68fdfbf73d19f0de07aaa3f0ae
Yash Kumar Sharma <yks.nit@gmail.com>
2022-12-31 00:13:59 +0400

    fix: update location of config

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <div data-id="internal-testing" />
     </div>
   );
 }
